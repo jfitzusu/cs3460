cf880f6f41aef66d3c5a7355634f1bacf16cb744
jfitzusu <A02261889@usu.edu>
2022-12-07 20:02:21 -0700

    Finished Windows Debugging

--- Assignment09/vector.cpp
+++ Assignment09/vector.cpp
@@ -1,2 +1,262 @@
 #include "vector.hpp"
 
+usu::vector<bool>::reference::reference(std::uint8_t* byte, std::uint8_t bit)
+{
+    if (bit > 7 || bit < 0)
+    {
+        throw new std::exception("Bit Not Within Byte");
+    }
+    m_location = byte;
+    m_bit = bit;
+}
+
+void usu::vector<bool>::reference::operator=(const bool& toSet)
+{
+    if (toSet)
+    {
+        *m_location |= 1 << m_bit;
+    }
+    else
+    {
+        *m_location &= ~(1 << m_bit);
+    }
+}
+
+usu::vector<bool>::reference::operator bool()
+{
+    return bool(*m_location >> m_bit & std::uint8_t(1));
+}
+
+bool usu::vector<bool>::reference::operator*()
+{
+    return bool(*m_location >> m_bit & std::uint8_t(1));
+}
+
+usu::vector<bool>::iterator::iterator() :
+    iterator(nullptr)
+{
+}
+
+usu::vector<bool>::iterator::iterator(pointer pointer) :
+    iterator(0, pointer){};
+
+usu::vector<bool>::iterator::iterator(size_type position, pointer pointer) :
+    m_pointer(pointer),
+    m_position(position){};
+
+usu::vector<bool>::iterator::iterator(const iterator& oldIter)
+{
+    this->m_position = oldIter.m_position;
+    this->m_pointer = oldIter.m_pointer;
+}
+
+usu::vector<bool>::iterator::iterator(iterator&& oldIter) noexcept
+{
+    this->m_position = oldIter.m_position;
+    this->m_pointer = oldIter.m_pointer;
+    oldIter.m_position = 0;
+    oldIter.m_pointer = nullptr;
+}
+
+usu::vector<bool>::iterator usu::vector<bool>::iterator::operator++()
+{
+    m_position++;
+    return *this;
+}
+
+usu::vector<bool>::iterator usu::vector<bool>::iterator::operator++(int)
+{
+    iterator i = *this;
+    m_position++;
+    return i;
+}
+
+usu::vector<bool>::iterator usu::vector<bool>::iterator::operator--()
+{
+    m_position--;
+    return *this;
+}
+
+usu::vector<bool>::iterator usu::vector<bool>::iterator::operator--(int)
+{
+    iterator i = *this;
+    m_position--;
+    return i;
+}
+
+usu::vector<bool>::iterator& usu::vector<bool>::iterator::operator=(const iterator& oldIter)
+{
+    this->m_position = oldIter.m_position;
+    this->m_pointer = oldIter.m_pointer;
+
+    return *this;
+}
+
+usu::vector<bool>::iterator& usu::vector<bool>::iterator::operator=(iterator&& oldIter)
+{
+    if (this != &oldIter)
+    {
+        std::swap(this->m_position, oldIter.m_position);
+        std::swap(this->m_pointer, oldIter.m_pointer);
+    }
+
+    return *this;
+}
+
+usu::vector<bool>::reference usu::vector<bool>::iterator::operator*()
+{
+    return reference(&m_pointer[m_position / 8], m_position % 8);
+}
+
+bool usu::vector<bool>::iterator::operator==(const iterator& iter)
+{
+    return m_position == iter.m_position;
+}
+
+bool usu::vector<bool>::iterator::operator!=(const iterator& iter)
+{
+    return m_position != iter.m_position;
+}
+
+usu::vector<bool>::vector() :
+    vector(M_DEFAULT_RESIZER)
+{
+}
+
+usu::vector<bool>::vector(size_type size) :
+    vector(size, M_DEFAULT_RESIZER)
+{
+}
+
+usu::vector<bool>::vector(resize_type resize) :
+    vector(0, resize){};
+
+usu::vector<bool>::vector(size_type size, resize_type resize) :
+    m_size(size),
+    m_capacity(M_DEFAULT_CAPACITY),
+    m_rawArray(nullptr),
+    m_resizer(resize)
+{
+    if (M_DEFAULT_CAPACITY < size)
+    {
+        m_capacity = size * 2;
+    }
+    m_rawArray = std::make_shared<std::uint8_t[]>((m_capacity - 1) / 8 + 1);
+};
+
+usu::vector<bool>::vector(std::initializer_list<bool> list) :
+    vector(list, M_DEFAULT_RESIZER)
+{
+}
+
+usu::vector<bool>::vector(std::initializer_list<bool> list, resize_type resize) :
+    m_size(0),
+    m_capacity(M_DEFAULT_CAPACITY),
+    m_rawArray(nullptr),
+    m_resizer(resize)
+{
+    m_rawArray = std::make_shared<std::uint8_t[]>((m_capacity - 1) / 8 + 1);
+    int current = 0;
+    for (auto i = list.begin(); i != list.end(); i++, current++)
+    {
+        add(*i);
+    }
+};
+
+usu::vector<bool>::reference usu::vector<bool>::operator[](size_type index)
+{
+    if (index >= m_size || index < 0)
+    {
+        throw std::range_error("Index Out of Bounds");
+    }
+    return reference(&m_rawArray[index / 8], index % 8);
+}
+
+void usu::vector<bool>::add(bool value)
+{
+    if (m_size >= m_capacity)
+    {
+        m_capacity = m_resizer(m_capacity);
+        pointer newArray = std::make_shared<std::uint8_t[]>((m_capacity - 1) / 8 + 1);
+        for (unsigned int i = 0; i < m_size / 8 + 1; i++)
+        {
+            newArray[i] = m_rawArray[i];
+        }
+        m_rawArray = newArray;
+    }
+
+    usu::vector<bool>::reference(&m_rawArray[m_size / 8], m_size % 8) = value;
+    m_size++;
+}
+
+void usu::vector<bool>::insert(size_type index, bool value)
+{
+    if (index > m_size || index < 0)
+    {
+        throw std::range_error("Index Out of Bounds");
+    }
+
+    if (m_size + 1 >= m_capacity)
+    {
+        m_capacity = m_resizer(m_capacity);
+        pointer newArray = std::make_shared<std::uint8_t[]>((m_capacity - 1) / 8 + 1);
+        for (unsigned int i = 0; i < m_size / 8 + 1; i++)
+        {
+            newArray[i] = m_rawArray[i];
+        }
+        m_rawArray = newArray;
+    }
+
+    m_size++;
+    bool toShift = *reference(&m_rawArray[index / 8], index % 8);
+    for (size_type i = index + 1; i < (m_size); i++)
+    {
+        bool nextShift = *reference(&m_rawArray[i / 8], i % 8);
+        reference(&m_rawArray[i / 8], i % 8) = toShift;
+        toShift = nextShift;
+    }
+
+    reference(&m_rawArray[index / 8], index % 8) = value;
+}
+
+void usu::vector<bool>::remove(size_type index)
+{
+    {
+        if (index >= m_size || index < 0)
+        {
+            throw std::range_error("Index Out of Bounds");
+        }
+
+        // If this works I STG
+        size_type tempMax = size_type(m_size);
+        m_size = index;
+        for (size_type i = index + 1; i < tempMax; i++)
+        {
+            add(reference(&m_rawArray[i / 8], i % 8));
+        }
+    };
+}
+
+usu::vector<bool>::size_type usu::vector<bool>::size()
+{
+    return m_size;
+}
+usu::vector<bool>::size_type usu::vector<bool>::capacity()
+{
+    return m_capacity;
+}
+
+void usu::vector<bool>::clear()
+{
+    m_size = 0;
+}
+
+usu::vector<bool>::iterator usu::vector<bool>::begin()
+{
+    return iterator(m_rawArray);
+}
+
+usu::vector<bool>::iterator usu::vector<bool>::end()
+{
+    return iterator(m_size, m_rawArray);
+}
--- Assignment09/vector.hpp
+++ Assignment09/vector.hpp
@@ -1,10 +1,11 @@
+#pragma once
+#include <exception>
+#include <functional>
 #include <iostream>
 #include <memory>
-#include <utility>
-#include <functional>
-#include <exception>
 #include <stdexcept>
 #include <typeinfo>
+#include <utility>
 
 namespace usu
 {
@@ -20,266 +21,58 @@ namespace usu
         using value_type = T;
         using resize_type = std::function<size_type(size_type)>;
 
-
-
-         class iterator
+        class iterator
         {
 
           public:
             using iterator_category = std::bidirectional_iterator_tag;
-            iterator() : iterator(nullptr)
-            {
-            };
-            iterator(pointer pointer) : iterator(0, pointer)
-            {
-            };
-            iterator(size_type position, pointer pointer) :
-                m_pointer(pointer),
-                m_position(position)
-            {
-            };
-
-            iterator(const iterator& oldIter)
-            {
-                this->m_position = oldIter.m_position;
-                this->m_pointer = oldIter.m_pointer;
-            }
-            iterator(iterator&& oldIter) noexcept
-            {
-                this->m_position = oldIter.m_position;
-                this->m_pointer = oldIter.m_pointer;
-                oldIter.m_position = 0;
-                oldIter.m_pointer = nullptr;
-            }
-
-            iterator operator++()
-            {
-                m_position++;
-                return *this;
-            }
-
-            iterator operator++(int)
-            {
-                iterator i = *this;
-                m_position++;
-                return i;
-            }
-
-            iterator operator--()
-            {
-                m_position--;
-                return *this;
-            }
-            iterator operator--(int)
-            {
-                iterator i = *this;
-                m_position--;
-                return i;
-            }
-            iterator& operator=(const iterator& oldIter)
-            {
-                this->m_position = oldIter.m_position;
-                this->m_pointer = oldIter.m_pointer;
-
-                return *this;
-            }
-            iterator& operator=(iterator&& oldIter) 
-                {
-                if (this != &oldIter)
-                {
-                    std::swap(this->m_position, oldIter.m_position);
-                    std::swap(this->m_pointer, oldIter.m_pointer);
-                }
-
-                return *this;
-            }
-
-
-            reference operator*()
-            {
-                return m_pointer[m_position];
-            };
-
-            T* operator->()
-            {
-                return &m_pointer[m_position];
-            };
-
-            bool operator==(const iterator& iter) 
-            { 
-                return m_position == iter.m_position; 
-            };
-            bool operator!=(const iterator& iter) 
-            { 
-                return m_position != iter.m_position; 
-            };
+            iterator();
+            iterator(pointer pointer);
+            iterator(size_type position, pointer pointer);
+            iterator(const iterator& oldIter);
+            iterator(iterator&& oldIter) noexcept;
+            iterator operator++();
+            iterator operator++(int);
+            iterator operator--();
+            iterator operator--(int);
+            iterator& operator=(const iterator& oldIter);
+            iterator& operator=(iterator&& oldIter);
+            reference operator*();
+            T* operator->();
+            bool operator==(const iterator& iter);
+            bool operator!=(const iterator& iter);
 
           private:
             size_type m_position;
             pointer m_pointer;
         };
 
-        vector() :
-            vector([](size_type currentCapacity) -> size_type
-                   {
-                       return currentCapacity * 2;
-                   })
-        {
-        }
-
-        vector(size_type size) :
-            vector(size, [](size_type currentCapacity) -> size_type
-                   {
-                       return currentCapacity * 2;
-                   })
-        {
-        }
-
-        vector(resize_type resize) :
-            vector(0, resize)
-        {
-        };
-
-        vector(size_type size, resize_type resize) :
-            m_size(size),
-            m_capacity(DEFAULT_CAPACITY),
-            m_rawArray(nullptr),
-            m_resizer(resize)
-        {
-            if (DEFAULT_CAPACITY < size)
-            {
-                m_capacity = size * 2;
-            }
-            m_rawArray = std::make_shared<T[]>(m_capacity);
-        };
-
-        vector(std::initializer_list<T> list) :
-            vector(list, [](size_type currentCapacity) -> size_type
-                   { return currentCapacity * 2; })
-        {
-        }
-
-        vector(std::initializer_list<T> list, resize_type resize) :
-            m_size(0),
-            m_capacity(DEFAULT_CAPACITY),
-            m_rawArray(nullptr),
-            m_resizer(resize)
-        {
-            m_rawArray = std::make_shared<T[]>(m_capacity);
-            int current = 0;
-            for (auto i = list.begin(); i != list.end(); i++, current++)
-            {
-                add(*i);
-            }
-        };
-
-        reference operator[](size_type index)
-        {
-            if (index >= m_size || index < 0)
-
-            {
-                throw new std::range_error("Index Out of Bounds");
-            }
-            return m_rawArray[index];
-        };
-
-        void add(T value)
-        {
-            if (m_size >= m_capacity)
-            {
-                m_capacity = m_resizer(m_capacity);
-                pointer newArray = std::make_shared<T[]>(m_capacity);
-                for (unsigned int i = 0; i < m_size; i++)
-                {
-                    newArray[i] = m_rawArray[i];
-                }
-                m_rawArray = newArray;
-            }
-
-            m_rawArray[m_size] = value;
-            m_size++;
-        };
-
-        void insert(size_type index, T value)
-        {
-            if (index > m_size || index < 0)
-            {
-                throw new std::range_error("Index Out of Bounds");
-            }
-
-
-            if (m_size + 1>= m_capacity)
-            {
-                m_capacity = m_resizer(m_capacity);
-                pointer newArray = std::make_shared<T[]>(m_capacity);
-                for (unsigned int i = 0; i < m_size; i++)
-                {
-                    newArray[i] = m_rawArray[i];
-                }
-                m_rawArray = newArray;
-            }
-
-            m_size++;
-            auto toShift = m_rawArray[index];
-            for (size_type i = index + 1; i < (m_size); i++) {
-                auto nextShift = m_rawArray[i];
-                m_rawArray[i] = toShift;
-                toShift = nextShift;
-            }
-           
-            m_rawArray[index] = value;
-
-        };
-
-        void remove(size_type index)
-        {
-            if (index >= m_size || index < 0)
-            {
-                throw new std::range_error("Index Out of Bounds");
-            }
-
-            // If this works I STG
-            size_type tempMax = size_type(m_size);
-            m_size = index;
-            for (size_type i = index + 1; i < tempMax; i++)
-            {
-                add(m_rawArray[i]);
-            }
-        };
-
-        void clear()
-        {
-            m_size = 0;
-        };
-
-        size_type size()
-        {
-            return m_size;
-        };
-
-        size_type capacity()
-        {
-            return m_capacity;
-        };
-
-        iterator begin()
-        {
-            return iterator(m_rawArray);
-        };
-
-        iterator end()
-        {
-            return iterator(m_size, m_rawArray);
-        };
-
-       
+        vector();
+        vector(size_type size);
+        vector(resize_type resize);
+        vector(size_type size, resize_type resize);
+        vector(std::initializer_list<T> list);
+        vector(std::initializer_list<T> list, resize_type resize);
+        reference operator[](size_type index);
+        void add(T value);
+        void insert(size_type index, T value);
+        void remove(size_type index);
+        void clear();
+        size_type size();
+        size_type capacity();
+        iterator begin();
+        iterator end();
 
       private:
         size_type m_size;
         size_type m_capacity;
         pointer m_rawArray;
         resize_type m_resizer;
-        static const size_type DEFAULT_CAPACITY = 10;
+        static const size_type M_DEFAULT_CAPACITY = 10;
+        static inline resize_type M_DEFAULT_RESIZER = [](size_type currentCapacity) -> size_type
+        {
+            return currentCapacity * 2;
+        };
     };
 
     template <>
@@ -287,68 +80,323 @@ namespace usu
     {
       public:
         using size_type = std::size_t;
-        using reference = bool;
         using pointer = std::shared_ptr<std::uint8_t[]>;
         using value_type = bool;
         using resize_type = std::function<size_type(size_type)>;
 
-         vector<bool>() :
-            vector([](size_type currentCapacity) -> size_type
-                   { return currentCapacity * 2; })
+        class reference
         {
-        }
 
-        vector<bool>(size_type size) :
-            vector(size, [](size_type currentCapacity) -> size_type
-                   { return currentCapacity * 2; })
-        {
-        }
+          public:
+            reference(std::uint8_t* byte, std::uint8_t bit);
+            void operator=(const bool& toSet);
+            operator bool();
+            bool operator*();
 
-        vector<bool>(resize_type resize) :
-            vector(0, resize){};
-
-        vector<bool>(size_type size, resize_type resize) :
-            m_size(size),
-            m_capacity(DEFAULT_CAPACITY),
-            m_rawArray(nullptr),
-            m_resizer(resize)
-        {
-            if (DEFAULT_CAPACITY < size)
-            {
-                m_capacity = size * 2;
-            }
-            m_rawArray = std::make_shared<std::uint8_t[]>((m_capacity - 1) / 8 + 1);
+          private:
+            std::uint8_t* m_location;
+            std::uint8_t m_bit;
         };
 
-        vector<bool>(std::initializer_list<bool> list) :
-            vector(list, [](size_type currentCapacity) -> size_type
-                   { return currentCapacity * 2; })
+        class iterator
         {
-        }
+          public:
+            using iterator_category = std::bidirectional_iterator_tag;
+            iterator();
+            iterator(pointer pointer);
+            iterator(size_type position, pointer pointer);
+            iterator(const iterator& oldIter);
+            iterator(iterator&& oldIter) noexcept;
+            iterator operator++();
+            iterator operator++(int);
+            iterator operator--();
+            iterator operator--(int);
+            iterator& operator=(const iterator& oldIter);
+            iterator& operator=(iterator&& oldIter);
+            reference operator*();
 
-        vector(std::initializer_list<bool> list, resize_type resize) :
-            m_size(0),
-            m_capacity(DEFAULT_CAPACITY),
-            m_rawArray(nullptr),
-            m_resizer(resize)
-        {
-            m_rawArray = std::make_shared<std::uint8_t[]>((m_capacity - 1) / 8 + 1);
-            int current = 0;
-            for (auto i = list.begin(); i != list.end(); i++, current++)
-            {
-                add(*i);
-            }
+            bool operator==(const iterator& iter);
+            bool operator!=(const iterator& iter);
+
+          private:
+            size_type m_position;
+            pointer m_pointer;
         };
+
+        vector<bool>();
+        vector<bool>(size_type size);
+        vector<bool>(resize_type resize);
+        vector<bool>(size_type size, resize_type resize);
+        vector<bool>(std::initializer_list<bool> list);
+        vector(std::initializer_list<bool> list, resize_type resize);
 
         reference operator[](size_type index);
-
+        void add(bool value);
+        void insert(size_type index, bool value);
+        void remove(size_type index);
+        size_type size();
+        size_type capacity();
+        void clear();
+        iterator begin();
+        iterator end();
 
       private:
         size_type m_size;
         size_type m_capacity;
         pointer m_rawArray;
         resize_type m_resizer;
-        static const size_type DEFAULT_CAPACITY = 10;
+        static const size_type M_DEFAULT_CAPACITY = 10;
+        static inline resize_type M_DEFAULT_RESIZER = [](size_type currentCapacity) -> size_type
+        {
+            return currentCapacity * 2;
+        };
     };
 
+    template <typename T>
+    vector<T>::iterator::iterator() :
+        iterator(nullptr){};
+
+    template <typename T>
+    vector<T>::iterator::iterator(pointer pointer) :
+        iterator(0, pointer){};
+
+    template <typename T>
+    vector<T>::iterator::iterator(size_type position, pointer pointer) :
+        m_pointer(pointer),
+        m_position(position){};
+
+    template <typename T>
+    vector<T>::iterator::iterator(const iterator& oldIter)
+    {
+        this->m_position = oldIter.m_position;
+        this->m_pointer = oldIter.m_pointer;
+    }
+
+    template <typename T>
+    vector<T>::iterator::iterator(iterator&& oldIter) noexcept
+    {
+        this->m_position = oldIter.m_position;
+        this->m_pointer = oldIter.m_pointer;
+        oldIter.m_position = 0;
+        oldIter.m_pointer = nullptr;
+    }
+
+    template <typename T>
+    vector<T>::iterator vector<T>::iterator::operator++()
+    {
+        m_position++;
+        return *this;
+    }
+
+    template <typename T>
+    vector<T>::iterator vector<T>::iterator::operator++(int)
+    {
+        iterator i = *this;
+        m_position++;
+        return i;
+    }
+
+    template <typename T>
+    vector<T>::iterator vector<T>::iterator::operator--()
+    {
+        m_position--;
+        return *this;
+    }
+
+    template <typename T>
+    vector<T>::iterator vector<T>::iterator::operator--(int)
+    {
+        iterator i = *this;
+        m_position--;
+        return i;
+    }
+
+    template <typename T>
+    vector<T>::iterator& vector<T>::iterator::operator=(iterator&& oldIter)
+    {
+        if (this != &oldIter)
+        {
+            std::swap(this->m_position, oldIter.m_position);
+            std::swap(this->m_pointer, oldIter.m_pointer);
+        }
+
+        return *this;
+    }
+
+    template <typename T>
+    vector<T>::reference vector<T>::iterator::operator*()
+    {
+        return m_pointer[m_position];
+    };
+
+    template <typename T>
+    T* vector<T>::iterator::operator->()
+    {
+        return &m_pointer[m_position];
+    };
+
+    template <typename T>
+    bool vector<T>::iterator::operator==(const iterator& iter)
+    {
+        return m_position == iter.m_position;
+    };
+
+    template <typename T>
+    bool vector<T>::iterator::operator!=(const iterator& iter)
+    {
+        return m_position != iter.m_position;
+    };
+
+    template <typename T>
+    vector<T>::vector() :
+        vector(M_DEFAULT_RESIZER)
+    {
+    }
+    template <typename T>
+    vector<T>::vector(size_type size) :
+        vector(size, M_DEFAULT_RESIZER)
+    {
+    }
+    template <typename T>
+    vector<T>::vector(resize_type resize) :
+        vector(0, resize){};
+    template <typename T>
+    vector<T>::vector(size_type size, resize_type resize) :
+        m_size(size),
+        m_capacity(M_DEFAULT_CAPACITY),
+        m_rawArray(nullptr),
+        m_resizer(resize)
+    {
+        if (M_DEFAULT_CAPACITY < size)
+        {
+            m_capacity = size * 2;
+        }
+        m_rawArray = std::make_shared<T[]>(m_capacity);
+    };
+    template <typename T>
+    vector<T>::vector(std::initializer_list<T> list) :
+        vector(list, M_DEFAULT_RESIZER)
+    {
+    }
+    template <typename T>
+    vector<T>::vector(std::initializer_list<T> list, resize_type resize) :
+        m_size(0),
+        m_capacity(M_DEFAULT_CAPACITY),
+        m_rawArray(nullptr),
+        m_resizer(resize)
+    {
+        m_rawArray = std::make_shared<T[]>(m_capacity);
+        int current = 0;
+        for (auto i = list.begin(); i != list.end(); i++, current++)
+        {
+            add(*i);
+        }
+    };
+
+    template <typename T>
+    vector<T>::reference vector<T>::operator[](size_type index)
+    {
+        if (index >= m_size || index < 0)
+
+        {
+            throw std::range_error("Index Out of Bounds");
+        }
+        return m_rawArray[index];
+    };
+
+    template <typename T>
+    void vector<T>::add(T value)
+    {
+        if (m_size >= m_capacity)
+        {
+            m_capacity = m_resizer(m_capacity);
+            pointer newArray = std::make_shared<T[]>(m_capacity);
+            for (unsigned int i = 0; i < m_size; i++)
+            {
+                newArray[i] = m_rawArray[i];
+            }
+            m_rawArray = newArray;
+        }
+
+        m_rawArray[m_size] = value;
+        m_size++;
+    };
+
+    template <typename T>
+    void vector<T>::insert(size_type index, T value)
+    {
+        if (index > m_size || index < 0)
+        {
+            throw std::range_error("Index Out of Bounds");
+        }
+
+        if (m_size + 1 >= m_capacity)
+        {
+            m_capacity = m_resizer(m_capacity);
+            pointer newArray = std::make_shared<T[]>(m_capacity);
+            for (unsigned int i = 0; i < m_size; i++)
+            {
+                newArray[i] = m_rawArray[i];
+            }
+            m_rawArray = newArray;
+        }
+
+        m_size++;
+        auto toShift = m_rawArray[index];
+        for (size_type i = index + 1; i < (m_size); i++)
+        {
+            auto nextShift = m_rawArray[i];
+            m_rawArray[i] = toShift;
+            toShift = nextShift;
+        }
+
+        m_rawArray[index] = value;
+    };
+
+    template <typename T>
+    void vector<T>::remove(size_type index)
+    {
+        if (index >= m_size || index < 0)
+        {
+            throw std::range_error("Index Out of Bounds");
+        }
+
+        // If this works I STG
+        size_type tempMax = size_type(m_size);
+        m_size = index;
+        for (size_type i = index + 1; i < tempMax; i++)
+        {
+            add(m_rawArray[i]);
+        }
+    };
+
+    template <typename T>
+    void vector<T>::clear()
+    {
+        m_size = 0;
+    };
+
+    template <typename T>
+    vector<T>::size_type vector<T>::size()
+    {
+        return m_size;
+    };
+
+    template <typename T>
+    vector<T>::size_type vector<T>::capacity()
+    {
+        return m_capacity;
+    };
+
+    template <typename T>
+    vector<T>::iterator vector<T>::begin()
+    {
+        return iterator(m_rawArray);
+    };
+
+    template <typename T>
+    vector<T>::iterator vector<T>::end()
+    {
+        return iterator(m_size, m_rawArray);
+    };
 } // namespace usu
